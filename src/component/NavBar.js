9a246a6dd18f86b571de2ebac9649386c48e43e7
valladaresassc <valladares.jose805@gmail.com>
2023-05-07 01:33:43 -0700

    ...

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
   return (
-    <Navbar style={{ color: "white", borderBottom: "px solid #ccc" }} variant="dark" expand="lg">
+    <Navbar style={{ color: "white", borderBottom: "px solid #ccc" }} variant="dark" expand="lg"  user={user} >
       <Navbar.Brand href="#" className="pl-3" style={{ font: "Roboto", fontSize: 21 }}>
-       <strong> Connect4 </strong>
+        <strong> Connect4 </strong>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/Home" style={{ font:  " Roboto" }}>
+          <Nav.Link href="/Home" style={{ font: " Roboto" }}>
             Home
           </Nav.Link>
           <Nav.Link href="/Account" style={{ font: "Roboto" }}>
@@ -23,10 +23,14 @@ const NavBar = () => {
             Game
           </Nav.Link>
         </Nav>
+        {user && user.username && (
+          <Nav.Link>
+            <p>Hello, {user.username}!</p>
+          </Nav.Link>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
 };
 
 export default NavBar;
-
