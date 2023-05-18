f0dcaf6fea4202af45b939461d06bdc9f8c7d8f9
valladaresassc <valladares.jose805@gmail.com>
2023-05-18 11:01:48 -0700

    ... leaderboard update

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -8,20 +8,40 @@ const Leaderboard = () => {
   useEffect(() => {
     // Retrieve the top 10 scores from Firestore
     const db = getFirestore();
-    const q = query(collection(db, 'users'), limit(10));
-    //, orderBy('score', 'desc')
-    getDocs(q).then((querySnapshot) => {
-      const results = [];
-      querySnapshot.forEach((doc) => {
-        results.push({ name: doc.data().username });
-        //, score: doc.data().score
+    const q = query(collection(db, 'users'), orderBy('wins', 'desc'), limit(10));
+    getDocs(q)
+      .then((querySnapshot) => {
+        const users = [];
+        let currentRank = 1;
+        let prevWins = null;
+
+        querySnapshot.forEach((doc) => {
+          const user = { name: doc.data().username, wins: doc.data().wins };
+
+          if (user.wins !== prevWins) {
+            // Update the rank only if the wins are different
+            user.rank = currentRank;
+          }
+
+          users.push(user);
+          prevWins = user.wins;
+          currentRank++;
+        });
+
+        // Fill in the blank ranks for tied users
+        for (let i = 0; i < users.length; i++) {
+          if (!users[i].rank) {
+            users[i].rank = users[i - 1].rank;
+          }
+        }
+
+        setData(users);
+      })
+      .catch((error) => {
+        console.error(error);
       });
-      setData(results);
-    }).catch((error) => {
-      console.error(error);
-    });
   }, []);
-  
+
   return (
     <div className="container">
       <h1 className="text-center">Leaderboard</h1>
@@ -36,9 +56,9 @@ const Leaderboard = () => {
         <tbody>
           {data.map((item, index) => (
             <tr key={index}>
-              <th scope="row">{index + 1}</th>
+              <th scope="row">{item.rank}</th>
               <td>{item.name}</td>
-              <td className="text-end">{item.score}</td>
+              <td className="text-end">{item.wins}</td>
             </tr>
           ))}
         </tbody>
